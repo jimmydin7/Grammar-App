282fe0e80e56d52d64504e40374bd216051433a4
Thbop <75861321+thbop@users.noreply.github.com>
2023-04-04 11:37:47 -0400

    Removed some comments and changed a color

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ class App(BaseApp, Index):
         super().__init__(__name__)
     
     def pages(self):
-        # self.test_page()
-        # self.test_page_name()
-        # self.test_input()
-        # self.test_process()
-
         self.index()
 
 if __name__ == '__main__':
