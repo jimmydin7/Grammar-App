ee44e33cb6ccccc5c539f859ca6b50b0c51ca7c6
jimmydin7 <129774378+jimmydin7@users.noreply.github.com>
2023-04-05 14:43:54 +0300

    Basic code to handle the try button in /about

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
-// Poor poor empty file...
+// Basic code to handle the "Try Grammar App" button in /about
+button_try = document.getElementById("try")
+
+button_try.addEventListener("click", function() {
+    window.location.href = "/";
+  });
+  
