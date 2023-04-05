2ef8d4f742895af4653c1a6e43cea0cbbe235973
jimmydin7 <129774378+jimmydin7@users.noreply.github.com>
2023-04-05 14:45:17 +0300

    Fixed an error/bug

--- scripts/server/user/db.py
+++ scripts/server/user/db.py
@@ -11,11 +11,16 @@ class Database:
             self.data = {}
         else:
             self.load()
-    
+
     def load(self):
         file = open(self.file)
         self.data = json.load(file)
         file.close()
+
+        # Check if the 'users' key exists, and create it if it doesn't
+        if 'users' not in self.data:
+            self.data['users'] = []
+
     def save(self):
         file = open(self.file, 'w')
         file.write(json.dumps(self.data))
