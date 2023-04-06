9d393ccac9556467d0e9041c6ec1b01a4662bea8
Thbop <75861321+thbop@users.noreply.github.com>
2023-04-06 10:53:50 -0400

    base.html merged + index.html looking fancy

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+
+// baad structure, I tried to do better...
+
+to_app = document.getElementById('to-app');
+
+to_app.addEventListener("click", function() {
+    window.location.href = "/grammar";
+});
+  
+
+support = document.getElementById('support');
+
+support.addEventListener("click", function() {
+    window.location.href = "/support";
+});
--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-// Basic code to handle the "Try Grammar App" button in /about
-
-function button_link(id)
-    button_try = document.getElementById("try")
-
-    button_try.addEventListener("click", function() {
-        window.location.href = "/";
-    });
-  
