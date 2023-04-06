92ddfbe3885be1a85387026988ef8cc23fea7170
Thbop <75861321+thbop@users.noreply.github.com>
2023-04-06 10:40:09 -0400

    Some incomplete migration to better front-end

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
 // Basic code to handle the "Try Grammar App" button in /about
-button_try = document.getElementById("try")
 
-button_try.addEventListener("click", function() {
-    window.location.href = "/";
-  });
+function button_link(id)
+    button_try = document.getElementById("try")
+
+    button_try.addEventListener("click", function() {
+        window.location.href = "/";
+    });
   
