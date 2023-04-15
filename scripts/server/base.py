3b1dabddbdaf2d80e2d1b9ad3378303343cf125b
Thbop <75861321+thbop@users.noreply.github.com>
2023-04-15 15:28:35 -0400

    Fixed some stupid code

--- scripts/server/base.py
+++ scripts/server/base.py
@@ -8,10 +8,7 @@ class BaseApp:
     
     
     def run(self, debug=False):
-        if debug:
-            self.app.run(debug=True, host="0.0.0.0", port=5000)
-        else:
-            self.app.run(debug=False, host="0.0.0.0", port=5000)
+        self.app.run(debug=debug, host="0.0.0.0", port=5000)
 
 if __name__ == '__main__':
     app = BaseApp()
